bbc933984e1462192c9e5a945dd6909eb8a1ff0a
hackasteroid142 <vicente.vega@usach.cl>
2021-02-03 14:45:12 -0300

    Update and delete added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show,:edit,:update,:destroy]
   
   def index
     @tasks = Task.all.order("created_at DESC")
@@ -9,7 +10,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(item_params)
+    @task = Task.new(item_params_new)
     if @task.save
       redirect_to root_path
     else 
@@ -21,12 +22,35 @@ class TasksController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+    if @task.update(item_params)
+      redirect_to root_path
+    else
+      render 'edit'
+    end 
+  end
+
+
   def destroy
+    @task.destroy
+    redirect_to root_path
   end
 
   private
 
-  def item_params
+  def item_params_new
     params.permit(:title)
   end 
+
+  def item_params
+    params.require(:task).permit(:title)
+  end 
+
+  def find_task
+     @task = Task.find(params[:id])
+  end
+
 end
